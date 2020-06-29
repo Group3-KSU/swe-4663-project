2860f8e5c243dafbe51028f7e6c707cac18ac07e
kehanks2 <50679506+kehanks2@users.noreply.github.com>
2020-06-29 15:04:10 -0400

    Update READ_ME.cs

--- ui_design/READ_ME.cs
+++ ui_design/READ_ME.cs
@@ -1,3 +1,4 @@
 // The files in the ui_design directory are just a quick little example I made with VS forms to show what the code looks like for forms.
-// Form1 is the home screen, where the user chooses to load a project or start a new one.
-// Form2 is the main screen.
+// Form1.cs is the home screen, where the user chooses to load a project or start a new one.
+// Form2.cs is the main screen.
+// The 2 files mentioned above are where the on click functions are.
