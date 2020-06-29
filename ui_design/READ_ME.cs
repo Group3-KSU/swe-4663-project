5a669af4c525eb90f1357f9aa2f0d33c89fd95b3
kehanks2 <50679506+kehanks2@users.noreply.github.com>
2020-06-29 14:58:57 -0400

    Create READ_ME.cs

---
+++ ui_design/READ_ME.cs
@@ -0,0 +1,3 @@
+// The files in the ui_design directory are just a quick little example I made with VS forms to show what the code looks like for forms.
+// Form1 is the home screen, where the user chooses to load a project or start a new one.
+// Form2 is the main screen.
